8c808673fe911626b996070b41c20482187e809f
bridgeren <240146036@qq.com>
2019-12-01 22:25:11 +0800

    inint rep

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# blog
+#####blog
+* 练手项目
+* 啥也没有
+* 毕竟菜鸡啥也不会
+
+##### 技术
+* SpringBoot +MyBatis+Mysql+Thymeleaf
+* BootStrap 
---
+++ src/main/java/com/nickless/blog/controller/Hello.java
@@ -0,0 +1,22 @@
+package com.nickless.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * creat by nickless
+ *
+ * @Date 2019/12/1 22:18
+ */
+@Controller
+public class Hello {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
