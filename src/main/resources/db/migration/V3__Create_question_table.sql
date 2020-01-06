1df05c2c7b6a1da4bca42be13c6b01aa9d751a2a
bridgeren <240146036@qq.com>
2020-01-06 15:47:16 +0800

    create question sql

---
+++ src/main/resources/db/migration/V3__Create_question_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE question
+(
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title varchar(50),
+    description TEXT,
+    gmt_create BIGINT,
+    gmt_modified BIGINT,
+    creator int,
+    comment_count int DEFAULT 0,
+    view_count int DEFAULT 0,
+    like_count int DEFAULT 0,
+    tag varchar(256)
+);
